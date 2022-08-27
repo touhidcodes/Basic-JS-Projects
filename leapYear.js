39c0b1ecd4c008e2a6b38a8523918f3799b5e2d5
touhidcodes <touhidcodes@gmail.com>
2022-08-27 21:13:31 +0600

    with string

--- leapYear.js
+++ leapYear.js
@@ -9,3 +9,17 @@ function leapYearCheck(year) {
 
 var checkYear = leapYearCheck(2017);
 console.log(checkYear);
+
+// Leap Year Check with string
+function leapYearCheck2(year) {
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    var leapyear = "This year is a Leap Year";
+    return leapyear;
+  } else {
+    var leapyearNot = "This year is not a Leap Year";
+    return leapyearNot;
+  }
+}
+
+var checkYear = leapYearCheck2(2020);
+console.log(checkYear);
