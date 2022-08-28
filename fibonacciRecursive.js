2fcdc33f83a8e563eb02f388204ed9b7cf2d1b73
touhidcodes <touhidcodes@gmail.com>
2022-08-28 21:13:05 +0600

    fibonacciRecursive.js

---
+++ fibonacciRecursive.js
@@ -0,0 +1,15 @@
+//Fibonacci Recursive
+function fibonacci(n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+  return fibonacci;
+}
+
+var output = fibonacci(13);
+console.log(output);
