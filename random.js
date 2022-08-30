70c6bc6e8f1ec8c234a6a281cbc4a849a4940ffe
touhidcodes <touhidcodes@gmail.com>
2022-08-30 23:53:12 +0600

    random.js

---
+++ random.js
@@ -0,0 +1,20 @@
+// Random
+var num = 2;
+for (i = 0; i < 10; i++) {
+  var randomNum = Math.random() * 10;
+  var output = Math.round(randomNum);
+  console.log(output);
+}
+
+// Math Functions
+var number = 9.49;
+console.log("Math Function is");
+
+var result1 = Math.floor(number);
+console.log(result1);
+
+var result2 = Math.ceil(number);
+console.log(result2);
+
+var result3 = Math.round(number);
+console.log(result3);
