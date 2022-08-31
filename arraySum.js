dd6fa2c329b5053328b7b2f59488e63ae22b1b0e
touhidcodes <touhidcodes@gmail.com>
2022-08-31 11:41:23 +0600

    arraySum.js

--- arraySum.js
+++ arraySum.js
@@ -1,10 +1,27 @@
 // Get Sum of Array
-var numbers = [20, 34, 45, 76, 98, 34, 89, 50, 90, 67, 34, 23];
+var result = [20, 34, 45, 76, 98, 34, 89, 50, 90, 67, 34, 23];
 var sum = 0;
 
-for (var i = 0; i < numbers.length; i++) {
-  var element = numbers[i];
+for (var i = 0; i < result.length; i++) {
+  var element = result[i];
   sum = sum + element;
 }
 
 console.log("Total of Numbers is: ", sum);
+
+// Function of Array Sum
+function getArraySum(numbers) {
+  sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
+    sum = sum + element;
+  }
+  return sum;
+}
+
+var numbers = [55, 65, 78, 96, 34, 56, 23, 67, 89, 08, 67, 56];
+var total = getArraySum(numbers);
+console.log("Total Number is: ", total);
+
+var total2 = getArraySum([55, 65, 78, 96, 56, 23, 67, 89, 08, 67, 56]);
+console.log("Total Number is: ", total2);
